a9bd863f0b7e370c2ab79862d65759bdc85464ad
arifrahmadi23 <muhammad231arif@gmail.com>
2023-07-02 04:49:41 +0700

    memperbaiki line 70

--- binarysearchtree_171/binarysearchtree_171.cpp
+++ binarysearchtree_171/binarysearchtree_171.cpp
@@ -67,7 +67,7 @@ public:
 			if (element < currentNode->info)
 				currentNode = currentNode->leftchild;
 			else
-				currentNode = currentNode->rightchild
+				currentNode = currentNode->rightchild;
 		}
 	}
 	
